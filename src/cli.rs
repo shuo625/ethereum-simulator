2aa569616dfda244c85f1b755fd6434f137c7134
shuo625 <qushuo233@gmail.com>
2022-07-03 00:24:17 +0100

    refine the variable name and add mod

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
 mod cmd;
+pub(self) mod cmd_errors;
 
 use std::io::{self, Write};
 
 use crate::state::State;
-use cmd::CmdExitCode;
 
 pub fn cli_run() {
     let mut state = State::new();
@@ -11,19 +11,12 @@ pub fn cli_run() {
         print!(">");
         io::stdout().flush().expect("flush failed");
 
-        let mut cmd = String::new();
+        let mut cmd_str = String::new();
 
         io::stdin()
-            .read_line(&mut cmd)
+            .read_line(&mut cmd_str)
             .expect("failed to read command");
 
-        let cmd_exit_code = cmd::cmd_run(cmd, &mut state);
-
-        match cmd_exit_code {
-            CmdExitCode::SUCC => {}
-            CmdExitCode::ERR => {
-                println!("wrong command");
-            }
-        }
+        cmd::cmd_exec(&cmd_str, &mut state);
     }
 }
