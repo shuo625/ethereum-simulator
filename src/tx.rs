29a535616a103d03332eab2334b11a8d33252447
shuo625 <qushuo233@gmail.com>
2022-07-03 20:55:55 +0100

    add functions of Tx to get fields

--- src/tx.rs
+++ src/tx.rs
@@ -1,23 +1,35 @@
 use sha256;
 
-use crate::eth_types::Address;
+// use crate::eth_types::Address;
 
 pub struct Tx {
-    from: Address,
-    to: Address,
+    from: String,
+    to: String,
     value: u64,
 }
 
 impl Tx {
-    pub fn new(from: Address, to: Address, value: u64) -> Self {
+    pub fn new(from: String, to: String, value: u64) -> Self {
         Tx { from, to, value }
+    }
+
+    pub fn from(&self) -> &str {
+        &self.from
+    }
+
+    pub fn to(&self) -> &str {
+        &self.to
+    }
+
+    pub fn value(&self) -> u64 {
+        self.value
     }
 
     pub fn hash(&self) -> String {
         sha256::digest(format!(
             "{}{}{}",
-            String::from_utf8(self.from.bytes().to_vec()).unwrap(),
-            String::from_utf8(self.to.bytes().to_vec()).unwrap(),
+            &self.from,
+            &self.to,
             self.value.to_string(),
         ))
     }
