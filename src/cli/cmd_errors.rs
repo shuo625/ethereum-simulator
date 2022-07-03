ef7b4e7b71ee401816c13768abf5ce467b943972
shuo625 <qushuo233@gmail.com>
2022-07-03 23:14:34 +0100

    add tx errors

--- src/cli/cmd_errors.rs
+++ src/cli/cmd_errors.rs
@@ -1,8 +1,15 @@
 pub enum CmdErrCode {
     INVALIDCMD,
     CMDACCOUNTERR(CmdAccountErrCode),
+    CMDTXERR(CmdTxErrCode),
 }
 
 pub enum CmdAccountErrCode {
     NOTEXISTEDACCOUNT,
+}
+
+pub enum CmdTxErrCode {
+    NOTEXISTEDFROM,
+    NOTEXISTEDTO,
+    NOTENOUGHBALANCE,
 }
