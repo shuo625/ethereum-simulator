9f8a83ea8c2a24f4e8805db8f71cb3bf8769c8e0
shuo625 <qushuo233@gmail.com>
2022-07-03 23:16:55 +0100

    add add and sub balance functions

--- src/account.rs
+++ src/account.rs
@@ -1,4 +1,5 @@
-use crate::eth_types::{Address, PrivateKey};
+// use crate::eth_types::{Address, PrivateKey};
+use crate::cli::cmd_errors::CmdTxErrCode;
 
 pub struct EoA {
     name: String,
@@ -22,5 +23,18 @@ impl EoA {
 
     pub fn get_balance(&self) -> u64 {
         self.balance
+    }
+
+    pub fn add_balance(&mut self, value: u64) {
+        self.balance += value;
+    }
+
+    pub fn sub_balance(&mut self, value: u64) -> Result<(), CmdTxErrCode> {
+        if self.balance >= value {
+            self.balance -= value;
+            Ok(())
+        } else {
+            Err(CmdTxErrCode::NOTENOUGHBALANCE)
+        }
     }
 }
