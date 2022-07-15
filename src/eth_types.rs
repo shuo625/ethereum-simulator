a68c23b858c09b48efd0f0bf63fe465e30aef638
shuo625 <qushuo233@gmail.com>
2022-07-15 22:27:55 +0100

    alias vec of u8 to bytes and create h256 type

--- src/eth_types.rs
+++ src/eth_types.rs
@@ -1,8 +1,14 @@
+use fixed_hash::construct_fixed_hash;
 use uint::construct_uint;
 
 pub type Address = ethsign::PublicKey;
 pub type PrivateKey = ethsign::SecretKey;
+pub type Bytes = Vec<u8>;
 
 construct_uint! {
     pub struct U256(4);
+}
+
+construct_fixed_hash! {
+    pub struct H256(32);
 }
