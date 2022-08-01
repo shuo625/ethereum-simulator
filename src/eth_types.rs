fc520e94ba408338c11156ec79d465c2c20f9d57
shuo625 <qushuo233@gmail.com>
2022-08-01 20:15:41 +0100

    reexport U256 and H256 and fix some value to mut

--- src/eth_types.rs
+++ src/eth_types.rs
@@ -1,8 +1,7 @@
+pub use ethereum_types::{H256, U256};
+
 pub type Address = ethsign::PublicKey;
 pub type PrivateKey = ethsign::SecretKey;
-
-pub struct U256;
-pub struct H256;
 
 pub mod num_op {
     pub fn u8s_to_u8(a: u8, b: u8) -> u8 {
@@ -11,7 +10,7 @@ pub mod num_op {
 
     // TODO: consider generic type
     pub fn u8s_to_u16(nums: &[u8]) -> u16 {
-        let value: u16 = 0;
+        let mut value: u16 = 0;
         let len = nums.len();
 
         for (i, &num) in nums.iter().enumerate() {
@@ -22,7 +21,7 @@ pub mod num_op {
     }
 
     pub fn u8s_to_u32(nums: &[u8]) -> u32 {
-        let value: u32 = 0;
+        let mut value: u32 = 0;
         let len = nums.len();
 
         for (i, &num) in nums.iter().enumerate() {
@@ -33,7 +32,7 @@ pub mod num_op {
     }
 
     pub fn u8s_to_u64(nums: &[u8]) -> u64 {
-        let value: u64 = 0;
+        let mut value: u64 = 0;
         let len = nums.len();
 
         for (i, &num) in nums.iter().enumerate() {
@@ -44,7 +43,7 @@ pub mod num_op {
     }
 
     pub fn u8s_to_u128(nums: &[u8]) -> u128 {
-        let value: u128 = 0;
+        let mut value: u128 = 0;
         let len = nums.len();
 
         for (i, &num) in nums.iter().enumerate() {
