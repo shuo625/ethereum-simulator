018a01d0181ee5c7ef9d086fce5ed97c7969eae9
shuo625 <qushuo233@gmail.com>
2022-08-07 13:12:11 +0100

    add a Code type and a trait FromStr containing a method from_str that generates a new Code from &str

--- src/eth_types.rs
+++ src/eth_types.rs
@@ -1,6 +1,29 @@
 pub use ethereum_types::{Address, Secret, H256, U256};
 
-pub type Bytes = Vec<u8>;
+// type for code
+pub type Code = Vec<u8>;
+
+pub trait FromStr {
+    fn from_str(code_str: &str) -> Self;
+}
+
+impl FromStr for Code {
+    fn from_str(code_str: &str) -> Self {
+        let mut code = Code::new();
+
+        let mut idx: usize = 0;
+        let bytes = code_str.as_bytes();
+
+        while idx < bytes.len() - 1 {
+            let byte_part_a = bytes[idx] - b'0';
+            let byte_part_b = bytes[idx + 1] - b'0';
+            code.push(num_op::u8s_to_u8(byte_part_a, byte_part_b));
+            idx += 2;
+        }
+
+        code
+    }
+}
 
 pub mod num_op {
     pub fn u8s_to_u8(a: u8, b: u8) -> u8 {
