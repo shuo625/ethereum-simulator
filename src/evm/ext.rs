0b72212d1ee4fddd322bb896b67f0b920be74c51
shuo625 <qushuo233@gmail.com>
2022-08-08 12:10:30 +0100

    fix bug make parameter account as mutable reference

--- src/evm/ext.rs
+++ src/evm/ext.rs
@@ -1,10 +1,13 @@
 use crate::{
-    account::Storage,
-    eth_types::{H256, U256},
+    account::{Account, Storage},
+    eth_types::{Address, H256, U256},
+    state::State,
     tx::Tx,
 };
 
 pub struct Ext<'a> {
+    state: &'a State,
+    account: &'a Account,
     storage: &'a mut Storage,
     tx: &'a Tx,
     chainid: usize,
@@ -12,9 +15,11 @@ pub struct Ext<'a> {
 }
 
 impl<'a> Ext<'a> {
-    pub fn new(storage: &'a mut Storage, tx: &'a Tx) -> Self {
+    pub fn new(state: &'a State, account: &'a mut Account, tx: &'a Tx) -> Self {
         Ext {
-            storage,
+            state,
+            account,
+            storage: account.get_mut_storage(),
             tx,
             chainid: 0,
             gas: 100,
@@ -40,5 +45,17 @@ impl<'a> Ext<'a> {
     pub fn get_callvalue(&self) -> U256 {
         let value = self.tx.value();
         U256::from(value)
+    }
+
+    pub fn get_address(&self) -> U256 {
+        U256::from(self.account.get_address().as_bytes())
+    }
+
+    pub fn get_balance(&self, address: Address) -> U256 {
+        U256::from(
+            self.state
+                .account_get_balance(&address.to_string())
+                .unwrap(),
+        )
     }
 }
