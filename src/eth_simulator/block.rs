5928c21cade1b28dad85f0cd59deb714864a06df
shuo625 <qushuo233@gmail.com>
2022-08-10 18:41:11 +0100

    refactor hash function of block

--- src/eth_simulator/block.rs
+++ src/eth_simulator/block.rs
@@ -1,15 +1,13 @@
-use sha256;
-
-use super::tx::Tx;
+use super::{eth_types::H256, hash, tx::Tx};
 
 pub struct Block {
     tx: Tx,
-    tx_hash: String,
-    prev_block_hash: String,
+    tx_hash: H256,
+    prev_block_hash: H256,
 }
 
 impl Block {
-    pub fn new(tx: Tx, prev_block_hash: String) -> Self {
+    pub fn new(tx: Tx, prev_block_hash: H256) -> Self {
         let tx_hash = tx.hash();
         Block {
             tx,
@@ -18,7 +16,7 @@ impl Block {
         }
     }
 
-    pub fn hash(&self) -> String {
-        sha256::digest(format!("{}{}", self.tx_hash, self.prev_block_hash))
+    pub fn hash(&self) -> H256 {
+        hash::keccak(format!("{}{}", self.tx_hash, self.prev_block_hash))
     }
 }
