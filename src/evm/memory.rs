cfaa0e6e622c71c9ed479d9f8346cfa1fb6784cf
shuo625 <qushuo233@gmail.com>
2022-08-10 09:11:49 +0100

    refactor style of codes

--- src/evm/memory.rs
+++ src/evm/memory.rs
@@ -14,29 +14,29 @@ impl Memory {
     }
 
     pub fn read(&self, offset: U256) -> U256 {
-        let off = offset.low_u64() as usize;
+        let off = offset.as_usize();
         U256::from(&self.memory[off..off + 32])
     }
 
-    pub fn read_slice(&self, offset: U256, size: U256) -> &[u8] {
-        let off = offset.low_u64() as usize;
-        let len = size.low_u64() as usize;
+    pub fn read_slice(&self, offset: U256, length: U256) -> &[u8] {
+        let off = offset.as_usize();
+        let len = length.as_usize();
 
         &self.memory[off..off + len]
     }
 
     pub fn write(&mut self, offset: U256, value: U256) {
-        let off = offset.low_u64() as usize;
+        let off = offset.as_usize();
         value.to_big_endian(&mut self.memory[off..off + 32]);
     }
 
     pub fn write_slice(&mut self, offset: U256, value: &[u8]) {
-        let off = offset.low_u64() as usize;
+        let off = offset.as_usize();
         self.memory[off..off + value.len()].copy_from_slice(value);
     }
 
     pub fn write_byte(&mut self, offset: U256, value: U256) {
-        let off = offset.low_u64() as usize;
+        let off = offset.as_usize();
         let val = value.low_u64() as u8;
         self.memory[off] = val;
     }
