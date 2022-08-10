425244d6614bf9c319fa56e09142c0ff15d6954a
shuo625 <qushuo233@gmail.com>
2022-08-10 22:24:47 +0100

    refactor codes to deal with error case

--- src/eth_simulator/evm/ext.rs
+++ src/eth_simulator/evm/ext.rs
@@ -7,6 +7,11 @@ use super::super::{
     tx::Tx,
 };
 
+pub enum ExtError {
+    NotExistedAccount(Address),
+    NotExistedStorageKey,
+}
+
 pub struct Ext<'a> {
     account: Address,
     accounts: &'a mut HashMap<Address, Account>,
@@ -33,29 +38,31 @@ impl<'a> Ext<'a> {
             .set_storage(key, value);
     }
 
-    pub fn get_storage(&self, key: &H256) -> H256 {
-        self.accounts.get(&self.account).unwrap().get_storage(key)
+    pub fn get_storage(&self, key: &H256) -> Result<H256, ExtError> {
+        match self.accounts.get(&self.account).unwrap().get_storage(key) {
+            Ok(v) => Ok(v),
+            Err(_) => Err(ExtError::NotExistedStorageKey),
+        }
     }
 
     pub fn get_gas(&self) -> U256 {
-        U256::from(self.gas)
+        U256::ethfrom(self.gas)
     }
 
     pub fn get_chainid(&self) -> U256 {
-        U256::from(self.chainid)
+        U256::ethfrom(self.chainid)
     }
 
     pub fn get_callvalue(&self) -> U256 {
-        let value = self.tx.value();
-        U256::from(value)
+        U256::ethfrom(self.tx.value())
     }
 
     pub fn get_address(&self) -> U256 {
         U256::ethfrom(&self.account)
     }
 
-    pub fn get_balance(&self, address: &Address) -> U256 {
-        U256::from(self.accounts.get(address).unwrap().get_balance())
+    pub fn get_balance(&self, address: &Address) -> Result<U256, ExtError> {
+        self.get_account_and_then(address, |account| U256::ethfrom(account.get_balance()))
     }
 
     pub fn get_origin(&self) -> U256 {
@@ -68,11 +75,11 @@ impl<'a> Ext<'a> {
 
     pub fn get_calldata(&self, i: U256) -> U256 {
         let idx = i.as_usize();
-        U256::from_big_endian(&self.tx.data()[idx..idx + 32])
+        U256::ethfrom(&self.tx.data()[idx..idx + 32])
     }
 
     pub fn get_calldatasize(&self) -> U256 {
-        U256::from(self.tx.data().len())
+        U256::ethfrom(self.tx.data().len())
     }
 
     pub fn get_calldata_slice(&self, offset: U256, length: U256) -> &[u8] {
@@ -82,28 +89,50 @@ impl<'a> Ext<'a> {
     }
 
     pub fn get_codesize(&self) -> U256 {
-        self.get_ext_codesize(&self.account)
+        self.get_ext_codesize(&self.account).unwrap_unchecked()
     }
 
     pub fn get_code_slice(&self, offset: U256, length: U256) -> &[u8] {
         self.get_ext_code_slice(&self.account, offset, length)
+            .unwrap_unchecked()
     }
 
     pub fn get_gasprice(&self) -> U256 {
-        U256::from(self.tx.gasprice())
+        U256::ethfrom(self.tx.gasprice())
     }
 
-    pub fn get_ext_codesize(&self, address: &Address) -> U256 {
-        U256::from(self.accounts.get(address).unwrap().get_code().len())
+    pub fn get_ext_codesize(&self, address: &Address) -> Result<U256, ExtError> {
+        self.get_account_and_then(address, |account| U256::ethfrom(account.get_code().len()))
     }
 
-    pub fn get_ext_code_slice(&self, address: &Address, offset: U256, length: U256) -> &[u8] {
+    pub fn get_ext_code_slice(
+        &self,
+        address: &Address,
+        offset: U256,
+        length: U256,
+    ) -> Result<&[u8], ExtError> {
         let off = offset.as_usize();
         let len = length.as_usize();
-        &self.accounts.get(address).unwrap().get_code()[off..off + len]
+
+        match self.accounts.get(address) {
+            Some(account) => Ok(&account.get_code()[off..off + len]),
+            None => Err(ExtError::NotExistedAccount(address.clone())),
+        }
     }
 
-    pub fn get_ext_code_hash(&self, address: &Address) -> U256 {
-        U256::ethfrom(hash::keccak(self.accounts.get(address).unwrap().get_code()))
+    pub fn get_ext_code_hash(&self, address: &Address) -> Result<U256, ExtError> {
+        self.get_account_and_then(address, |account| {
+            U256::ethfrom(hash::keccak(account.get_code()))
+        })
+    }
+
+    fn get_account_and_then<T, F>(&self, address: &Address, f: F) -> Result<T, ExtError>
+    where
+        F: FnOnce(&Account) -> T,
+    {
+        match self.accounts.get(address) {
+            Some(account) => Ok(f(account)),
+            None => Err(ExtError::NotExistedAccount(address.clone())),
+        }
     }
 }
