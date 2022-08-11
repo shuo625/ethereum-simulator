cf003300ae64280a0709265b2da209c89b16d876
shuo625 <qushuo233@gmail.com>
2022-08-11 09:43:19 +0100

    design api of eth simulator

--- src/api.rs
+++ src/api.rs
@@ -1,2 +1,15 @@
 mod cli;
 mod rpc;
+
+pub struct AccountInfo {
+    name: String,
+    address: String,
+    balance: usize,
+}
+pub trait EthApi {
+    fn account_add(&mut self, name: &str);
+    fn account_list(&self) -> Vec<AccountInfo>;
+    fn account_balance(&self) -> usize;
+
+    fn tx_send(&mut self, params_file: &str);
+}
