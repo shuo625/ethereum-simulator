41a9604850896dffa990302d61d588a3d3fd9bed
shuo625 <qushuo233@gmail.com>
2022-08-11 16:37:48 +0100

    define the api of the simulator and impl

--- src/api.rs
+++ src/api.rs
@@ -2,14 +2,20 @@ mod cli;
 mod rpc;
 
 pub struct AccountInfo {
-    name: String,
-    address: String,
-    balance: usize,
+    pub name: String,
+    pub address: String,
+    pub balance: usize,
+}
+
+pub enum EthError {
+    NotExistedAddress,
+    NotEnoughBalance,
+    VMError,
 }
 pub trait EthApi {
     fn account_add(&mut self, name: &str);
     fn account_list(&self) -> Vec<AccountInfo>;
-    fn account_balance(&self) -> usize;
+    fn account_balance(&self, address: &str) -> Result<usize, EthError>;
 
-    fn tx_send(&mut self, params_file: &str);
+    fn tx_send(&mut self, params_file: &str) -> Result<(), EthError>;
 }
--- src/eth_simulator.rs
+++ src/eth_simulator.rs
@@ -5,3 +5,77 @@ mod evm;
 mod hash;
 mod state;
 mod tx;
+
+use self::{
+    eth_types::{Address, EthFrom},
+    state::{State, StateError},
+};
+use crate::api::{AccountInfo, EthApi, EthError};
+
+pub struct EthSimulator {
+    state: State,
+}
+
+impl EthSimulator {
+    pub fn new() -> Self {
+        EthSimulator {
+            state: State::new(),
+        }
+    }
+}
+
+impl EthApi for EthSimulator {
+    fn account_add(&mut self, name: &str) {
+        self.state.account_add(name);
+    }
+
+    fn account_list(&self) -> Vec<AccountInfo> {
+        self.state
+            .account_list()
+            .iter()
+            .map(|(name, address, balance)| AccountInfo {
+                name: name.to_string(),
+                address: address.to_string(),
+                balance: *balance,
+            })
+            .collect()
+    }
+
+    fn account_balance(&self, address: &str) -> Result<usize, EthError> {
+        match self.state.account_get_balance(&Address::ethfrom(address)) {
+            Ok(value) => Ok(value),
+            Err(StateError::NotExistedAddress(addr)) => {
+                #[cfg(debug_assertions)]
+                println!("{} not existed", addr);
+
+                Err(EthError::NotExistedAddress)
+            }
+        }
+    }
+
+    fn tx_send(&mut self, params_file: &str) -> Result<(), EthError> {
+        match self.state.tx_send(params_file) {
+            Ok(_) => Ok(()),
+            Err(err) => match err {
+                StateError::NotExistedAddress(address) => {
+                    #[cfg(debug_assertions)]
+                    println!("{} not existed", address);
+
+                    Err(EthError::NotExistedAddress)
+                }
+                StateError::NotEnoughBalance => {
+                    #[cfg(debug_assertions)]
+                    println!("not enough balance");
+
+                    Err(EthError::NotEnoughBalance)
+                }
+                StateError::VMError(vm_error) => {
+                    #[cfg(debug_assertions)]
+                    println!("vm error: {:#?}", vm_error);
+
+                    Err(EthError::VMError)
+                }
+            },
+        }
+    }
+}
