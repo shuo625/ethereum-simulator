c12888e43c52ab1e93119b2e04f3e455c8aa33f2
shuo625 <qushuo233@gmail.com>
2022-08-12 17:13:39 +0100

    build the cli application

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-use ethereum_simulator::cli_run;
+use clap::{Parser, ValueEnum};
+use ethereum_simulator::Client;
 
 fn main() {
-    cli_run();
+    let args = Args::parse();
+
+    let client = Client::new(&args.mode.to_string()).unwrap();
+    client.run();
+}
+
+#[derive(Parser)]
+#[clap(version)]
+struct Args {
+    #[clap(short, long, arg_enum, value_parser)]
+    mode: Mode,
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+enum Mode {
+    REPL,
+    Rpc,
+}
+
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
