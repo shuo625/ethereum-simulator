a65a541eb09cddd73124ff0efd0bee8221c96c59
shuo625 <qushuo233@gmail.com>
2022-08-12 13:20:19 +0100

    add client struct

--- src/client.rs
+++ src/client.rs
@@ -1,2 +1,24 @@
 mod cli;
 mod rpc;
+
+pub enum Client {
+    Cli(cli::Cli),
+    Rpc(rpc::Rpc),
+}
+
+impl Client {
+    pub fn new(arg: &str) -> Option<Self> {
+        match arg {
+            "cli" => Some(Self::Cli(cli::Cli::new())),
+            "rpc" => Some(Self::Rpc(rpc::Rpc::new())),
+            _ => None,
+        }
+    }
+
+    pub fn run(self) {
+        match self {
+            Self::Cli(cli) => cli.run(),
+            Self::Rpc(rpc) => rpc.run(),
+        }
+    }
+}
