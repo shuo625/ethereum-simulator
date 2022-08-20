6ddb76687884da3d20467d2b6d7e378e51d1816b
shuo625 <qushuo233@gmail.com>
2022-08-20 23:30:01 +0100

    change type of file argument to &Path

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
 pub mod solc {
-    use std::{error::Error, path::PathBuf, process::Command};
+    use std::{error::Error, path::Path, process::Command};
 
     const COMPILER: &'static str = "solc";
 
-    pub fn compile(file: PathBuf) -> Result<String, Box<dyn Error>> {
-        let command = Command::new(COMPILER).arg("--bin").arg(file).output()?;
+    pub fn compile(file: &Path) -> Result<String, Box<dyn Error>> {
+        let command = Command::new(COMPILER)
+            .arg("--bin")
+            .arg(file.to_str().unwrap())
+            .output()?;
         let output = String::from_utf8(command.stdout).unwrap();
         let lines: Vec<&str> = output.lines().collect();
 
