ec014d279228c55f02f799c4c16e5e2a9ee0d0f9
shuo625 <qushuo233@gmail.com>
2022-08-20 15:46:26 +0100

    update the example solidity program

--- tests/example.sol
+++ tests/example.sol
@@ -1,8 +1,30 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.5.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
 
-contract C {
-    function echo(uint32 x) public pure returns (uint32) {
-        return x;
+// This is just a simple example of a coin-like contract.
+// It is not ERC20 compatible and cannot be expected to talk to other
+// coin/token contracts.
+
+contract MetaCoin {
+    mapping(address => uint32) balances;
+
+    constructor() {
+        balances[tx.origin] = 10000;
+    }
+
+    function sendCoin(address receiver, uint32 amount)
+        public
+        returns (bool sufficient)
+    {
+        if (balances[tx.origin] < amount) return false;
+        balances[tx.origin] -= amount;
+        balances[receiver] += amount;
+
+        return true;
+    }
+
+    function getBalance(address addr) public view returns (uint32) {
+        return balances[addr];
     }
 }
