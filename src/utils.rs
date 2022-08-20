f4832311a3380a496b9893705f3acff3d11bc31f
shuo625 <qushuo233@gmail.com>
2022-08-20 23:22:46 +0100

    fix bug

--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,8 @@ pub mod solc {
     const COMPILER: &'static str = "solc";
 
     pub fn compile(file: PathBuf) -> Result<String, Box<dyn Error>> {
-        let command = Command::new(COMPILER).arg("--bin").arg(file);
-        let output = String::from_utf8(command.output()?.stdout).unwrap();
+        let command = Command::new(COMPILER).arg("--bin").arg(file).output()?;
+        let output = String::from_utf8(command.stdout).unwrap();
         let lines: Vec<&str> = output.lines().collect();
 
         Ok(String::from(lines[3]))
