f92d3ae1acf7b32ba06c1929bbdeb4dfcb55b2da
shuo625 <qushuo233@gmail.com>
2022-08-21 18:30:44 +0100

    add a new test contract

---
+++ tests/Echo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
+
+// This is just a simple example of a coin-like contract.
+// It is not ERC20 compatible and cannot be expected to talk to other
+// coin/token contracts.
+
+contract Echo {
+    function echo(uint32 x) public view returns (uint32) {
+        return x;
+    }
+}
