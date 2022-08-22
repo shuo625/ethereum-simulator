d8301ea58630eb416411573df760c188dfe9e7bf
shuo625 <qushuo233@gmail.com>
2022-08-22 11:44:26 +0100

    add some tests

--- tests/client.py
+++ tests/client.py
@@ -1,12 +1,6 @@
-# frontend -> backend(contract) -> blockchain client
-#          <-         <-
-# frontend -> backend(contract) -> simulator
-#          <-         <-
-
 import socket
 import json
 
-# 0xc86e8742966300101238f1a5eb834530bbc7689a
 add_bob = {
     "method": "account_add",
     "params": {
@@ -14,7 +8,6 @@ add_bob = {
     }
 }
 
-# 0x93746dcfda94c0566ac76dc7255ce526c94122a3
 add_jack = {
     "method": "account_add",
     "params": {
@@ -27,20 +20,98 @@ account_list = {
     "params": {}
 }
 
-tx = {
-    "from": "0xc86e8742966300101238f1a5eb834530bbc7689a",
-    "to": "",
-    "value": "0",
-    "data": "608060405234801561001057600080fd5b5060b68061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063901717d114602d575b600080fd5b60336047565b604051603e91906067565b60405180910390f35b60006001905090565b6000819050919050565b6061816050565b82525050565b6000602082019050607a6000830184605a565b9291505056fea26469706673582212208237f441dea9dad1ed0b8161380ea48ff7cab71e3524abc1f52cf551d5e220e764736f6c634300080f0033"
+deploy_metacoin = {
+    "method": "contract_deploy",
+    "params": {
+        "from": "bob",
+        "contract_file": "tests/MetaCoin.sol"
+    }
 }
 
-tx_send = {
-    "method": "tx_send",
-    "params": tx
+call_metacoin_getbalance_bob = {
+    "method": "contract_call",
+    "params": {
+        "from": "bob",
+        "contract": "MetaCoin",
+        "input": "f8b2cb4f000000000000000000000000e89d37315c02ae1b66b058b5f621b834c959b704"
+    }
 }
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect(('127.0.0.1', 8000))
-    client.sendall(json.dumps(add_bob).encode(encoding='utf-8'))
-    result = client.recv(1024).decode(encoding='utf-8')
-    print(result)
+call_metacoin_getbalance_jack = {
+    "method": "contract_call",
+    "params": {
+        "from": "bob",
+        "contract": "MetaCoin",
+        "input": "f8b2cb4f0000000000000000000000002ef081cee51cf6c4e628ec9056fcc82ff68616a2"
+    }
+}
+
+call_metacoin_sendcoin = {
+    "method": "contract_call",
+    "params": {
+        "from": "bob",
+        "contract": "MetaCoin",
+        "input": "68d4b9c90000000000000000000000002ef081cee51cf6c4e628ec9056fcc82ff68616a200000000000000000000000000000000000000000000000000000000000000c8"
+    }
+}
+
+
+def account_add():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(add_bob).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(add_jack).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+
+def call_contract():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(deploy_metacoin).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(
+            call_metacoin_getbalance_bob).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(
+            call_metacoin_sendcoin).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(
+            call_metacoin_getbalance_bob).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', 8000))
+
+        client.sendall(json.dumps(
+            call_metacoin_getbalance_jack).encode(encoding='utf-8'))
+        result = client.recv(1024).decode(encoding='utf-8')
+        print(json.loads(result))
+
+
+if __name__ == '__main__':
+    call_contract()
